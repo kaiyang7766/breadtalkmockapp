60cee304d8650d03381336f51e7181def69e201a
Kai Yang <65525083+kaiyang7766@users.noreply.github.com>
2021-07-08 23:53:26 +0800

    Update Navbar.js

--- src/js/Cake/Navbar.js
+++ src/js/Cake/Navbar.js
@@ -9,19 +9,19 @@ export default function CakeNavbar() {
         <nav>
             <ul>
                 <li>
-                    <Link to='/cake/trace'>Traceability System 追溯系统</Link>
+                    <Link to='/cake/trace' id='selectdepartment'>Traceability System 追溯系统</Link>
                 </li>
                 <li>
-                    <Link to='/cake/cleaning'>Cleaning and sanitation</Link>
+                    <Link to='/cake/cleaning' className="disableCursor" onClick={ (event) => event.preventDefault()}>Cleaning and sanitation</Link>
                 </li>
                 <li>
-                    <Link to='/cake/metal'>Metal detector</Link>
+                    <Link to='/cake/metal' className="disableCursor" onClick={ (event) => event.preventDefault()}>Metal detector</Link>
                 </li>
                 <li>
-                    <Link to='/cake/cooking'>Cooking</Link>
+                    <Link to='/cake/cooking' className="disableCursor" onClick={ (event) => event.preventDefault()}>Cooking</Link>
                 </li>
                 <li>
-                    <Link to='/cake/chiller'>Chiller and Freezer</Link>
+                    <Link to='/cake/chiller' className="disableCursor" onClick={ (event) => event.preventDefault()}>Chiller and Freezer</Link>
                 </li>
             </ul>
         </nav>
