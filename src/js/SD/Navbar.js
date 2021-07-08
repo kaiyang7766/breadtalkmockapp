97ccf17b0249277a90912d712c566b2d2ffcf665
Kai Yang <65525083+kaiyang7766@users.noreply.github.com>
2021-07-08 23:58:04 +0800

    Update Navbar.js

--- src/js/SD/Navbar.js
+++ src/js/SD/Navbar.js
@@ -9,16 +9,16 @@ export default function SdNavbar() {
         <nav>
             <ul>
                 <li>
-                    <Link to='/SD/trace'>Traceability System 追溯系统</Link>
+                    <Link to='/SD/trace' id='selectdepartment'>Traceability System 追溯系统</Link>
                 </li>
                 <li>
-                    <Link to='/SD/cleaning'>Cleaning and sanitation</Link>
+                    <Link to='/SD/cleaning' className="disableCursor" onClick={ (event) => event.preventDefault()}>Cleaning and sanitation</Link>
                 </li>
                 <li>
-                    <Link to='/SD/metal'>Metal detector</Link>
+                    <Link to='/SD/metal' className="disableCursor" onClick={ (event) => event.preventDefault()}>Metal detector</Link>
                 </li>
                 <li>
-                    <Link to='/SD/chiller'>Chiller and Freezer</Link>
+                    <Link to='/SD/chiller' className="disableCursor" onClick={ (event) => event.preventDefault()}>Chiller and Freezer</Link>
                 </li>
             </ul>
         </nav>
